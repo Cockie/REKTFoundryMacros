53cff92682ab57bee19edba7bc23de9775795a15
Yorick <cockie.vdb@gmaol.com>
2022-01-15 12:56:10 +0100

    updooted blast grenade

--- Blast Grenade.js
+++ Blast Grenade.js
@@ -1,15 +1,27 @@
-//THIS MACRO NEED THE WARPGATE MODULE AND IS INTENDED FOR FIREBALL ONLY.
-//
+let selected = canvas.tokens.controlled[0];
+let tactor = GetSheetActor(event);
+if (!selected || selected.actor != tactor) {
+    let tokens = canvas.tokens.placeables;
+    if (tokens.length > 0) {
+        for (let i = 0; i < tokens.length; i++) {
+            if (tokens[i].actor == tactor) {
+                selected = tokens[i];
+                break;
+            }
+        }
+    }
+}
+if (!selected) {
+    ui.notifications.warn("No token found for this character");
+    return;
+}
+let ammo = GetUsesOfcItemForToken(selected, "Blast Grenade");
+if (ammo < 1) {
+    ui.notifications.warn("No enough ammo for Blast Grenade");
+    return;
+}
 
-//TO USE THIS MACRO, you'll need Wasp's "Sequencer" module and Honeybadger's Wargate module
-//You can find its documentation of Sequencer here on Github : https://github.com/fantasycalendar/FoundryVTT-Sequencer
-//There is also a wiki with a lot more info if you want to dive into it : https://github.com/fantasycalendar/FoundryVTT-Sequencer/wiki
 
-//You can use the "Show Sequencer Database" Tool in the toolbar of Foundry to preview all our Fireball explosion animation.
-//Thanks to the module Sequencer, you'll also be able to copy the path of the asset you want in a single click and replace it here to customize the macro.
-
-//It will play the animation of exploding fireball leaving cracks on the ground and choose the right file corresponding to the distance between the caster and the selected position. 
-//Don't forget to select the caster before using this macro.
 
 
 let config = {
@@ -23,9 +35,37 @@ let config = {
     rememberControlled: true 
 }
 
-
-const selected = canvas.tokens.controlled[0];
+// pick position
 let position = await warpgate.crosshairs.show(config);
+
+// check if token has citem   
+let citem = tactor.data.data.citems.find(y => y.name.startsWith("Blast Grenade"));
+if (citem != null) {
+    let gitem = game.items.find(y => y.name == citem.name);
+    if (gitem != null) {
+        //make the actual roll
+        let fitem = await game.items.get(citem.id);
+        let rollexp = fitem.data.data.roll;
+        let rollname = fitem.data.data.rollname;
+        let rollid = [fitem.data.data.rollid];
+        let rollcitemID = citem.id;
+        let actorattributes = tactor.data.data.attributes;
+        let citemattributes = citem.attributes;
+        let number = citem.number;
+        let targets = game.user.targets.ids;
+        let useData = {};
+        useData.id = citem.id;
+        useData.value = citem.isactive;
+        if (citem.usetype == "CON") {
+            useData.iscon = true;
+        }
+        tactor._sheet.rollExpression(rollexp, rollname, rollid, actorattributes, citemattributes, number, rollcitemID, targets, null, useData);
+    }
+} else {
+    ui.notifications.warn('The token(' + token.data.name + ') does not have the cItem ' + scItemName);
+}
+
+//play animation
 let rot = Math.floor(Math.random() * 360);
 let sequence = new Sequence()
 
@@ -59,3 +99,51 @@ let sequence = new Sequence()
         .name("Grenade_Blast")
         .persist()
 .play();
+
+function GetSheetActor(event) {
+    let returnactor;
+    let actorid = '';
+    let cp = event.composedPath();
+    for (let key in cp) {
+        if (cp.hasOwnProperty(key)) {
+            if ((typeof (cp[key]) !== "undefined" && cp[key] !== null)) {
+                if ((typeof (cp[key].classList) !== "undefined" && cp[key].classList !== null)) {
+                    if (cp[key].classList.contains('sandbox') && cp[key].classList.contains('sheet') && cp[key].classList.contains('actor')) {
+                        //console.log(cp[key].id);  //actor-MMwTr94GekOCihrC   or actor-MMwTr94GekOCihrC-6bX8wMQkdZ9OyOQa
+                        // get the id from the div
+                        actorid = cp[key].id.substring(6);  // length of 'actor-'            
+                        // if the actorid is double(ie MwTr94GekOCihrC-6bX8wMQkdZ9OyOQa), then it is a unlinked token, 
+                        // need to get the actor from the canvas so use the secondary id            
+                        if (actorid.length > 16) {
+                            // get secondary actor id
+                            actorid = actorid.substring(17);
+                            //console.log('Token Actor ' + actorid);
+                            //token = canvas.tokens.get(actorid);
+                            let token = canvas.tokens.placeables.find(y => y.id == actorid);
+                            if (token != null) {
+                                returnactor = token.actor;
+                            }
+                        } else {
+                            //console.log('Actor ' + actorid);   // MwTr94GekOCihrC
+                            returnactor = game.actors.get(actorid);
+                        }
+                        // exit for loop
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    return returnactor;
+}
+
+function GetUsesOfcItemForToken(token, scItemName) {
+    // check if token has citem   
+    let actor = token.actor;
+    let citem = actor.data.data.citems.find(y => y.name.startsWith(scItemName));
+    if (citem) {
+        return citem.uses;
+    } else {
+        return 0;
+    }
+}
